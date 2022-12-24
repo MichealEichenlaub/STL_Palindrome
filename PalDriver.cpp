e5297b35f728368ed54ecc4fbb681b25cfb78517
MichealEichenlaub <michenlaub@gmail.com>
2022-12-24 19:35:28 +0000

    Bug Fix

--- PalDriver.cpp
+++ PalDriver.cpp
@@ -1,48 +1,111 @@
+//Quick practice program that checks to see if a word is a
+// palindrome (reads the same foward and back) uses STL
+// queue and stack
+
+
 #include <iostream>
 #include <stack>
 #include <queue>
 #include <string>
 using namespace std;
 
-bool palcheck(stack<char> pstack, queue<char> pqueue);
+bool palCheck(stack<char> &pstack, queue<char> &pqueue);
+void loadCheck(stack<char> &pstack, queue<char> &pqueue, string toLoad);
+
+
+
+
 
 int main(){
 
     stack<char> pals;
     queue<char> palq;
-    string palstring = "madam";
 
 
-    for (int i = 0; i < palstring.size(); i++){
-        palq.push(palstring[i]);
-        pals.push(palstring[i]);
-    }
+    string palstring = "kayak"; // kayak should return as a palindrome
+    loadCheck(pals,palq,palstring);
 
     
 
-    if(palcheck(pals,palq) == true){
-        cout << palstring << " is a palindrome\n";
-    }
-    else{
-        cout << palstring << " is not a palindrome\n";
+
+    if(palCheck(pals,palq) == true){
+        cout << endl <<palstring << " is a palindrome\n\n";
+    }                                                 
+    else{                                              
+        cout << endl << palstring << " is not a palindrome\n\n";
     }
 
-   
+
+
+   palstring = "Seattle"; // Seattle should return as not a palindrome
+   loadCheck(pals,palq,palstring);
+
+
+
+
+    if(palCheck(pals,palq) == true){
+        cout << palstring << " is a palindrome\n\n";
+    }                                                  
+    else{                                             
+        cout << palstring << " is not a palindrome\n\n";
+    }
+
+
+
+
+    palstring = "bob"; // Bob should return as a palindrome
+    loadCheck(pals,palq,palstring);
+
+
+
+    if(palCheck(pals,palq) == true){
+        cout << palstring << " is a palindrome\n\n";
+    }                                                  
+    else{                                              
+        cout << palstring << " is not a palindrome\n\n";
+    }
+    
 
     return 0;
 }
 
-bool palcheck(stack<char> pstack, queue<char> pqueue){
+
+
+
+//-----------------------------------------------------------------------------
+// Checks for whether or not a world is a palindrome by popping the top of the 
+// stack and comparing it to the front of the queue. 
+// if they are equal, return true. If they are not, return false. 
+bool palCheck( stack<char> &pstack,queue<char> &pqueue){
 
         for(int i = 0; i < pstack.size(); i++){
 
             if(pstack.top() != pqueue.front()){
-                cout << "Not a palindrome\n";
+                while( !pqueue.empty()){
+                    pqueue.pop();
+                }
+                
+                while (!pstack.empty()){
+                    pstack.pop();
+                }
+                
                 return false;
             }
 
-
+            pstack.pop();
+            pqueue.pop();
         }
-    return true;
 
+    return true;
+}
+
+
+void loadCheck(stack<char> &pstack, queue<char> &pqueue, string toLoad){
+
+      for (int i = 0; i < toLoad.size(); i++){
+        
+        pstack.push(toLoad[i]);
+        pqueue.push(toLoad[i]);
+       
+    }
 }
